9aa0eb771dcb6fcbae72441f8b0b42fe9c234946
Josh Gearhart <joshua.gearhart@thomsonreuters.com>
2018-04-04 15:50:08 -0400

    Added selectors

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -1,44 +1,58 @@
-import * as fromPizzas from '../actions/pizzas.action';
+import * as fromPizzas from "../actions/pizzas.action";
 import { Pizza } from "../../models/pizza.model";
 
 export interface PizzaState {
-  data: Pizza[],
-  loaded: boolean,
-  loading: boolean,
+  data: Pizza[];
+  loaded: boolean;
+  loading: boolean;
 }
 
 export const initialState: PizzaState = {
-  data: [],
+  data: [
+    {
+      name: "Plain Ol' Pepperoni",
+      toppings: [
+        {
+          id: 10,
+          name: "pepperoni"
+        }
+      ],
+      id: 3
+    }
+  ],
   loaded: false,
-  loading: false,
+  loading: false
 };
 
 export function reducer(
   state = initialState,
   action: fromPizzas.PizzaAction
-  ): PizzaState {
-
-    switch(action.type){
-      case fromPizzas.LOAD_PIZZAS:{
-        return {
-          ...state,
-          loading: true
-        };
-      }
-      case fromPizzas.LOAD_PIZZAS_FAIL:{
-        return {
-          ...state,
-          loading: false,
-          loaded: false
-        };
-      }
-      case fromPizzas.LOAD_PIZZAS_SUCCESS:{
-        return {
-          ...state,
-          loading: false,
-          loaded: true
-        };
-      }
+): PizzaState {
+  switch (action.type) {
+    case fromPizzas.LOAD_PIZZAS: {
+      return {
+        ...state,
+        loading: true
+      };
     }
+    case fromPizzas.LOAD_PIZZAS_FAIL: {
+      return {
+        ...state,
+        loading: false,
+        loaded: false
+      };
+    }
+    case fromPizzas.LOAD_PIZZAS_SUCCESS: {
+      return {
+        ...state,
+        loading: false,
+        loaded: true
+      };
+    }
+  }
+  return state;
+}
 
-};
+export const getPizzasLoading = (state: PizzaState) => state.loading;
+export const getPizzasLoaded = (state: PizzaState) => state.loaded;
+export const getPizzas = (state: PizzaState) => state.data;
